f2fdf8da0f386ab27ff1d6b038dd158d36a5b94d
Nicholas Cooke <ncooke3@gatech.edu>
2020-03-06 21:11:38 -0500

    Initial Commit

---
+++ Objc Fluid Interfaces/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Objc Fluid Interfaces
+//
+//  Created by Nicholas Cooke on 3/6/20.
+//  Copyright © 2020 Nicholas Cooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Objc Fluid Interfaces/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objc Fluid Interfaces
+//
+//  Created by Nicholas Cooke on 3/6/20.
+//  Copyright © 2020 Nicholas Cooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
